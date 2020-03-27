ba0c4e8567ecc4560075ff5900bf09dd45a05703
Nehemias Alcantara <nemiasalc56@gmail.com>
2020-03-27 00:06:36 -0500

    added a register method

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+
 class App extends Component {
-  
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      loggedIn: false,
+      user: ''
+    }
+  }
+
+
+  // register method
+  register = (registerInfo) => {
+
+    console.log("this is the register method being called");
+  }
+
+
+
   render() {
+
     return (
     <div className="App">
       <h1>Social Community</h1>
