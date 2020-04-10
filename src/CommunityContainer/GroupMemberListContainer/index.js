9a5abda784053b30d27d2b5109cadfc43e4ac9cc
Nehemias Alcantara <nemiasalc56@gmail.com>
2020-04-09 19:23:50 -0500

    added a get members method

--- src/CommunityContainer/GroupMemberListContainer/index.js
+++ src/CommunityContainer/GroupMemberListContainer/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Modal } from 'semantic-ui-react'
+import './GroupMemberList.css'
 
 
 class GroupMemberListContainer extends Component {
@@ -11,17 +12,27 @@ class GroupMemberListContainer extends Component {
 		}
 	}
 
+	componentDidMount() {
+		this.getMembers()
+	}
 
 	// close modal
 	close = () => {
 		this.props.switcher()
 	}
 
+	getMembers = () => {
+		console.log("trying to get members");
+	}
+
 	render() {
 
 		return(
-			<Modal open={this.state.open} onClose={this.close}>
-				<h3>GroupMemberListContainer</h3>
+			<Modal size="mini" open={this.state.open} onClose={this.close}>
+				<div className="member-list">
+					<h3>GroupMemberListContainer</h3>
+					
+				</div>
 			</Modal>
 			)
 
