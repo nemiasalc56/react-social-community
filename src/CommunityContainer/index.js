35ea2687dc7d95d1ac5d0f4a7c0a7932ee0fdd54
Nehemias Alcantara <nemiasalc56@gmail.com>
2020-04-18 15:10:53 -0500

    added a logout method

--- src/CommunityContainer/index.js
+++ src/CommunityContainer/index.js
@@ -319,6 +319,9 @@ class Community extends Component {
 		}
 	}
 
+	logout = () => {
+		console.log("user is trying to logout");
+	}
 
 
 	render() {
@@ -331,6 +334,7 @@ class Community extends Component {
 					<Dropdown text={this.state.user.first_name}>
 					    <Dropdown.Menu>
 							<Dropdown.Item text='Update Account' onClick={()=> this.openUpdateAccount()}/>
+							<Dropdown.Item text='Logout' onClick={()=> this.logout()} />
 					    </Dropdown.Menu>
 					</Dropdown>
 
