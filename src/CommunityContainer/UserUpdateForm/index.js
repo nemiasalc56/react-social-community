37c387318821ac509ab247172c3b9de32e842034
Nehemias Alcantara <nemiasalc56@gmail.com>
2020-04-17 21:08:32 -0500

    user can see an update form

--- src/CommunityContainer/UserUpdateForm/index.js
+++ src/CommunityContainer/UserUpdateForm/index.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react'
+import { Form, Button, Modal } from 'semantic-ui-react'
 
 
 
 class UserUpdateForm extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			open: true,
+			first_name: '',
+			last_name: '',
+			password: ''
+		}
+	}
 
 
 	render() {
@@ -10,6 +21,34 @@ class UserUpdateForm extends Component {
 		return(
 			<div>
 				UserUpdateForm
+
+				<Modal size="mini" open={this.state.open} >
+					<h3>UserUpdateForm</h3>
+					
+					<Form>
+						<Form.Input 
+							label="First name"
+							placeholder="First name"
+							value={this.state.first_name}
+						/>
+
+						<Form.Input 
+							label="Last name"
+							placeholder="Last name"
+							value={this.state.last_name}
+						/>
+
+						<Form.Input 
+							label="Password"
+							placeholder="Password"
+							value={this.state.password}
+						/>
+
+						<Button type="submit">Update Account</Button>
+
+					</Form>
+
+				</Modal>
 			</div>
 			)
 	}
